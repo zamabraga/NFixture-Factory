0096dcd8efb152de41e1464433305f04aecc6095
zamabandeira <zama.braga@eldorado.org.br>
2015-07-29 16:55:21 -0300

    fix the project description

--- NFixtureFactory/Properties/AssemblyInfo.cs
+++ NFixtureFactory/Properties/AssemblyInfo.cs
@@ -1,11 +1,12 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle ("Fixture-Factory")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyTitle("NFixtureFactory")]
+[assembly: AssemblyDescription("Fixture Factory .NET is a tool to help developers quickly build and organize fake objects for unit tests. The key idea is to create specification limits of the data (templates) instead of hardcoded data.")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
@@ -17,10 +18,12 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.1")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyFileVersionAttribute("1.00.0.1")]
+[assembly: GuidAttribute("52cde8ae-2d19-4fc2-a2e6-0edef3447d62")]
