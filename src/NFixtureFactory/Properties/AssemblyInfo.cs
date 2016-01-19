586949afd94c9ffb779b7f57416b8f11a76c2db9
Zama Bandeira Braga <zama.braga@live.com>
2016-01-19 10:01:23 -0200

    update assembly version

--- src/NFixtureFactory/Properties/AssemblyInfo.cs
+++ src/NFixtureFactory/Properties/AssemblyInfo.cs
@@ -18,12 +18,12 @@ using System.Runtime.InteropServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.0.6")]
+[assembly: AssemblyVersion("1.0.0.7")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
-[assembly: AssemblyFileVersionAttribute("1.0.0.6")]
+[assembly: AssemblyFileVersionAttribute("1.0.0.7")]
 [assembly: GuidAttribute("52cde8ae-2d19-4fc2-a2e6-0edef3447d62")]
